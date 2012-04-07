b8f3d84e2a459acf1a8c995f7fdac6404fad8bb9
Fabian <tslpublisher@gmail.com>
2012-04-08 05:28:06 +1200

    v.0.2

---
+++ src/com/nz/thesmartlemon/tap/Preferences.java
@@ -0,0 +1,12 @@
+package com.nz.thesmartlemon.tap;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Preferences extends Activity {
+
+	public void onCreate(Bundle b){
+		super.onCreate(b);
+		setContentView(R.layout.pref);
+	}
+}
